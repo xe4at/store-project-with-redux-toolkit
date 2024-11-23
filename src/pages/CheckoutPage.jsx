9ad01e92faa748d427b9b79dd9363f54c0d5acf2
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-11-23 22:35:54 +0330

    show data in basketSidbar

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,33 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import BasketCard from "../components/BasketCard";
 import BasketSidebar from "../components/BasketSidebar";
-// import { useCart } from "../context/CardContext";
 import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
-  // const [state, dispatch] = useCart();
+  const state = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
 
-  // const clickHandler = (type, payload) => dispatch({ type, payload });
-
-  // if (!state.itemCounter) {
-  //   return (
-  //     <div className={styles.container}>
-  //     <p className={styles.e}>Empty</p>
-  //     </div>
-  //   );
-  // }
-
+  if (state.itemCounte) {
+    return (
+      <div className={styles.container}>
+        <p className={styles.e}>Empty</p>
+      </div>
+    );
+  }
   return (
     <div className={styles.container}>
-      {/* <BasketSidebar state={state} clickHandler={clickHandler} />
+      <BasketSidebar state={state} />
       <div className={styles.products}>
         {state.selectedItems.map((product) => (
-          <BasketCard
-            key={product.id}
-            data={product}
-            clickHandler={clickHandler}
-          />
+          <BasketCard key={product.id} data={product} />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
